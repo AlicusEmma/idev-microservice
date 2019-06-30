3b3345c10b4877473fcf631ea41c6aeef1f764be
Alicus <alicus890824@foxmail.com>
2019-06-30 14:24:51 +0800

    Dev Status: Done Dev Note  : Init idev MicroService Framework

---
+++ src/main/java/com/idev/iDevApplication.java
@@ -0,0 +1,22 @@
+package com.idev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaServer
+public class iDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(iDevApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello iDev Register Application, based on Spring Boot Framework!";
+    }
+}
